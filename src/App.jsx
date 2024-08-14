754a8af7db29f95945da0ddb5eaec0efcb39b9ef
DEVANSHI BAVARIA <bavariadevanshi@gmail.com>
2024-08-14 15:01:35 +0530

    commit message

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import LinkButton from './components/LinkButton/LinkButton';
+import Navbar from './components/Navbar/Navbar';
+import CardsContainer from './components/ProjectList/CardsContainer';
+import ScrollUpButton from './components/ScrollUpButton/ScrollUpButton';
+import SocialShare from './components/SocialShare/SocialShare';
+
+const App = () => {
+  return (
+    <div className="App">
+        <Navbar />
+        <div className="App-header">
+            <h1>Make your first open source contribution in 5 minutes</h1>
+        </div>
+        <ScrollUpButton />
+        <LinkButton />
+        <CardsContainer />
+        <SocialShare/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ProjectList/ProjectsCards.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./css/project-cards.css";
+
+const Card = ({
+  projectLink,
+  logoLink,
+  name,
+  description,
+  tags: propsTags,
+}) => {
+  let tags = [];
+
+  if (propsTags) {
+    tags = propsTags.map((tag, key) => (
+      <div key={key}>
+        <p>{tag}</p>
+      </div>
+    ));
+  }
+
+  return (
+    <div className="Card-Container">
+      <a className="Card-Real-Link" href={projectLink} target='blank'>
+        <div className="Card-Header">
+          <img
+            className="Project-Logo"
+            alt="the framework or language that the project is build upon"
+            src={logoLink}
+          />
+          <p className="Card-Title">{name}</p>
+        </div>
+        <div className="Card-Body">
+          <div className="Card-Tag">{tags}</div>
+          <div className="Card-Description">
+            <p> {description}</p>
+          </div>
+        </div>
+        <div className="Card-Link">Go to Project</div>
+      </a>
+    </div>
+  );
+};
+
+export default Card;
